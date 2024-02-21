51869d2c39a47af713e4be84e3e378186a2e0bea
Alina Kabanets <kabanets.dolls@gmail.com>
2024-02-21 15:15:39 +0000

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        element.addEventListener(eventName, ()=>{
+            // eslint-disable-next-line max-len
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const notification = document.createElement("div");
+    notification.className = "message";
+    notification.textContent = message;
+    document.body.append(notification);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
